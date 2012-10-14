dd67072aca7aad9980c067bc7a0e4c5f0a5c218d
eliasb <eliasb@whole-enchilada.mit.edu>
2012-10-13 22:38:37 -0400

    Add string representations for Person and Crush

--- crush_connector/models.py
+++ crush_connector/models.py
@@ -5,6 +5,12 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
 
+    def __str__(self):
+        return self.name
+
 class Crush(models.Model):
     crusher = models.ForeignKey(Person, related_name='crush_crushers')
     crushee = models.ForeignKey(Person, related_name='crush_crushees')
+
+    def __str__(self):
+        return "%s likes %s" % (self.crusher, self.crushee)
