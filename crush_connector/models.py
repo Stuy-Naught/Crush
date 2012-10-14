f369f895e68ee2d6e5f5ac9fae90cf2b47ea7fe4
Blake M Elias <eliasb@mit.edu>
2012-10-13 22:41:31 -0400

    Use __unicode__ for string representation instead of __str__

--- crush_connector/models.py
+++ crush_connector/models.py
@@ -5,12 +5,12 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
 class Crush(models.Model):
     crusher = models.ForeignKey(Person, related_name='crush_crushers')
     crushee = models.ForeignKey(Person, related_name='crush_crushees')
 
-    def __str__(self):
+    def __unicode__(self):
         return "%s likes %s" % (self.crusher, self.crushee)
