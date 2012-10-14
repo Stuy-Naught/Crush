59c0cde16681366f62c73a24f718fc40766f42e4
Blake M Elias <eliasb@mit.edu>
2012-10-13 22:31:50 -0400

    Add email field for Person model

--- crush_connector/models.py
+++ crush_connector/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=200)
+    email = models.EmailField()
 
 class Crush(models.Model):
     crusher = models.ForeignKey(Person, related_name='crush_crushers')
