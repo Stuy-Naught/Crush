4b27b6a1842b0f6920dd2cabad67416710e842dc
eliasb <eliasb@whole-enchilada.mit.edu>
2012-10-16 23:18:13 -0400

    Create blank template

--- crush_connector/views.py
+++ crush_connector/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.template import Context, loader
+from crush_connector.models import Person, Crush
+from django.http import HttpResponse
+
+def register(request):
+    t = loader.get_template('crush_connector/index.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
+
