0e7f284387b391cae6c9cd3ec29ee7ec41978546
Blake M Elias <eliasb@mit.edu>
2012-10-27 22:59:45 -0400

    Parse person's info and return it in JSON

--- name_lookup/views.py
+++ name_lookup/views.py
@@ -11,8 +11,12 @@ def lookup_mit_people(request, string):
     fixed_string = re.sub('@mit.edu', '', fixed_string)
     response = urllib2.urlopen('http://web.mit.edu/bin/cgicso?options=general&query=%s' % fixed_string)
     response_string = ' '.join([s[:-1] for s in response.readlines()])
-    root = ET.fromstring(response_string)
-    node = root.find('pre')
-    text = node.text
-    #crush_info = re.match('<pre>\(.*\)</pre>', response_string).group()
-    return HttpResponse(text)
+    info = re.match('.*<PRE>(.*)</PRE>', response_string).group(1)
+    m = re.match('\s*name:(.*)\s*email: <A.*>(.*)</A>\s*address: (.*)\s*year: (.*)', info)
+
+    name = m.group(1).strip()
+    email = m.group(2).strip()
+    address = m.group(3).strip()
+    year = m.group(4).strip()
+    
+    return HttpResponse('{"name": "%s", "email": "%s", "address": "%s", "year": "%s"}' % (name, email, address, year))
