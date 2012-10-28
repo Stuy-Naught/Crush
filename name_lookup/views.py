82f92424ff60344c12905470f847d2d0bcfa7a9d
Blake M Elias <eliasb@mit.edu>
2012-10-27 21:10:48 -0400

    Remove @mit.edu from email addresses because MIT People won't process it

--- name_lookup/views.py
+++ name_lookup/views.py
@@ -7,6 +7,7 @@ from crush_connector.forms import RegisterForm
 
 def lookup_mit_people(request, string):
     fixed_string = re.sub(' ', '+', string)
+    fixed_string = re.sub('@mit.edu', '', fixed_string)
     response = urllib2.urlopen('http://web.mit.edu/bin/cgicso?options=general&query=%s' % fixed_string)
     response_string = ' '.join(response.readlines())
     return HttpResponse(response_string)
