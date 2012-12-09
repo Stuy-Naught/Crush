1529168118ecaf474c347ea2212a4692b6ac5bdf
Blake M Elias <eliasb@mit.edu>
2012-12-09 17:06:48 -0500

    Revert to normal email field for validation

--- crush_connector/forms.py
+++ crush_connector/forms.py
@@ -21,6 +21,6 @@ class RegisterForm(forms.Form):
         super(RegisterForm, self).__init__(*args, **kwargs)
         
         for i in range(Crush.num_allowed_crushes):
-            self.fields['Crush_email_%d' % (i+1)] = MITEmailField(required=(i==0), initial="Start by typing a name or email")
+            self.fields['Crush_email_%d' % (i+1)] = EmailField(required=(i==0), initial="Start by typing a name or email")
             
             # require at least one crush email, but not the others
