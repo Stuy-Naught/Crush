aef258352f0d92c962341c0439f584bcf5582711
Blake M Elias <eliasb@mit.edu>
2012-12-11 13:46:00 -0500

    Put up splash page

--- crush/urls.py
+++ crush/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'crush_connector.views.index'),
+    url(r'^preview/$', 'crush_connector.views.index'),
+    url(r'^$', 'crush_connector.views.splash'),
     url(r'^auth/', 'crush_connector.views.auth'),
     url(r'^ctest/', 'ctest.views.index'),
     url(r'^about/', 'crush_connector.views.about'),
