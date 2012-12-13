e9858b1b72e53561fb2fcb04f418f9d18f2bc39d
Blake M Elias <eliasb@mit.edu>
2012-12-13 11:59:04 -0500

    Commands to clear for testing, and count mutual crushes.

---
+++ crush_connector/management/commands/clear_for_testing.py
@@ -0,0 +1,25 @@
+from crush_connector.models import Person, Crush, PersonBeenNotified
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+  def handle_noargs(self, *args, **kwargs):
+    tester_1 = Person.objects.get(email='eliasb@mit.edu')
+    tester_2 = Person.objects.get(email='whaack@mit.edu')
+    crushes_1 = Crush.objects.filter(crusher=tester_1, crushee=tester_2)
+    crushes_2 = Crush.objects.filter(crusher=tester_2, crushee=tester_1)
+    for crush in crushes_1:
+      crush.delete()
+    for crush in crushes_2:
+      crush.delete()
+    
+    notified_1 = PersonBeenNotified.objects.filter(person=tester_1)
+    notified_2 = PersonBeenNotified.objects.filter(person=tester_2)
+    for notified in notified_1:
+      notified.delete()
+    for notified in notified_2:
+      notified.delete()
+
+    tester_1.num_crushes_used = 0
+    tester_2.num_crushes_used = 0
+    tester_1.save()
+    tester_2.save()
