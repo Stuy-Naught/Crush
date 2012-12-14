6ff3e9529cc4d7777eae900973a70b54471eaa40
Blake M Elias <eliasb@mit.edu>
2012-12-14 06:09:56 -0500

    Hash crush emails only twice

--- crush_connector/hash_crushes.py
+++ crush_connector/hash_crushes.py
@@ -2,8 +2,8 @@ from models import CrushHash
 from Crypto.Hash import SHA512
 
 def crush_digest(Person1, Person2):
-    s1 = multi_hash(Person1.email, 1017)
-    s2 = multi_hash(Person2.email, 1239)
+    s1 = multi_hash(Person1.email, 2)
+    s2 = multi_hash(Person2.email, 2)
     s = '%s | %s' % (s1, s2)
     s_hash = multi_hash(s, 10273)
     return s_hash
