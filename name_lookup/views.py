79f11d1c298f3618a3f358261e5d4505d3516cad
Blake Elias <blakeelias@gmail.com>
2015-10-25 00:17:58 -0400

    Get rid of unicode tag in front of strings

--- name_lookup/views.py
+++ name_lookup/views.py
@@ -99,7 +99,9 @@ def create_autocomplete_file():
     for person in Person.objects.all():
         autocompleteList.append({'label': '%s - %s' % (person.name, person.email), 'value': person.email})
     autocompleteFile = file('/afs/athena.mit.edu/user/w/h/whaack/Scripts/django/crush/templates/crush_connector/names.json', 'w')
-    autocompleteFile.write(str(autocompleteList))
+    #autocompleteFile.write(str(autocompleteList))
+    for item in autocompleteList:
+        autocompleteFile.write(str("{'label': %s, 'value': %s},\n" % (str(item.label), str(item.value))))
     autocompleteFile.close()
 
 def name_lookup_and_populate():
